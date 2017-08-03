3256f446fbdb6e0b06821a396046eb89cf3316aa
Yefim Yevtushenko <webevt@users.noreply.github.com>
2017-08-03 20:05:42 +0300

    BB-11193: Check out and cancel with Apruve integration periodically fails (maintenance/2.3) (#12239)
    * BB-11193: Check out and cancel with Apruve integration periodically fails
    - fixed ensure that apruve library is loaded before starting apruve checkout
    - removed skip tag from behat feature
    
    * BB-11193: Check out and cancel with Apruve integration periodically fails
    - moved loading mask lower
    
    * BB-11193: Check out and cancel with Apruve integration periodically fails
    - added waitForAjax to ensure that page is fully loaded after redirect

--- Resources/public/js/app/components/payment-method-component.js
+++ Resources/public/js/app/components/payment-method-component.js
@@ -36,7 +36,7 @@ define(function(require) {
         },
 
         /**
-         * @param {Apruve} apruve
+         * @param {apruve} apruve
          */
         initializeApruve: function(apruve) {
             this.apruve = apruve;
@@ -67,11 +67,16 @@ define(function(require) {
                 this.returnUrl = responseData.returnUrl;
                 this.errorUrl = responseData.errorUrl;
 
-                // Provide order object and secure hash to Apruve.
-                this.apruve.setOrder(responseData.apruveOrder, responseData.apruveOrderSecureHash);
+                var self = this;
+                // Ensure that apruve library is loaded before starting apruve checkout.
+                this.deferredInit.done(function() {
+                    // Provide order object and secure hash to apruve.
+                    self.apruve.setOrder(responseData.apruveOrder, responseData.apruveOrderSecureHash);
 
-                mediator.execute('hideLoading');
-                this.apruve.startCheckout();
+                    self.apruve.startCheckout();
+
+                    mediator.execute('hideLoading');
+                });
             }
         },
 
