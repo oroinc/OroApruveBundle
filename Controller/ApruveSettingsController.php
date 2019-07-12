e9815ed71e2a6c605c4f3d1548ed7c0f1694271a
Mykhailo Sulyma <msulima@oroinc.com>
2019-07-12 17:18:47 +0300

    BAP-19112: Fix failed unit tests  - updated classes which still use removed interfaces  - misc failed unit tests fixed

--- Controller/ApruveSettingsController.php
+++ Controller/ApruveSettingsController.php
@@ -16,7 +16,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Translation\TranslatorInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 /**
  * Apruve Settings Controller
