c8b66a27abd830b495f4b577b346e231417087ea
Vova Soroka <vsoroka@oroinc.com>
2023-03-18 02:11:38 +0200

    Refactor unit tests in PayPalExpressBundle (#35096)

--- Tests/Unit/PaymentTransaction/Action/PaymentTransactionInvoiceActionTest.php
+++ Tests/Unit/PaymentTransaction/Action/PaymentTransactionInvoiceActionTest.php
@@ -6,12 +6,57 @@ use Oro\Bundle\ApruveBundle\Method\ApruvePaymentMethod;
 use Oro\Bundle\ApruveBundle\PaymentTransaction\Action\PaymentTransactionInvoiceAction;
 use Oro\Bundle\PaymentBundle\Entity\PaymentTransaction;
 use Oro\Bundle\PaymentBundle\Method\PaymentMethodInterface;
-use Oro\Component\Testing\Unit\EntityTrait;
+use Oro\Bundle\PaymentBundle\Method\Provider\PaymentMethodProviderInterface;
+use Oro\Bundle\PaymentBundle\Provider\PaymentTransactionProvider;
+use Oro\Component\ConfigExpression\ContextAccessor;
+use Oro\Component\Testing\ReflectionUtil;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\OptionsResolver\Exception\UndefinedOptionsException;
 use Symfony\Component\PropertyAccess\PropertyPath;
+use Symfony\Component\Routing\RouterInterface;
 
-class PaymentTransactionInvoiceActionTest extends AbstractActionTest
+class PaymentTransactionInvoiceActionTest extends \PHPUnit\Framework\TestCase
 {
-    use EntityTrait;
+    /** @var ContextAccessor|\PHPUnit\Framework\MockObject\MockObject */
+    private $contextAccessor;
+
+    /** @var PaymentMethodProviderInterface|\PHPUnit\Framework\MockObject\MockObject */
+    private $paymentMethodProvider;
+
+    /** @var PaymentTransactionProvider|\PHPUnit\Framework\MockObject\MockObject */
+    private $paymentTransactionProvider;
+
+    /** @var RouterInterface|\PHPUnit\Framework\MockObject\MockObject */
+    private $router;
+
+    /** @var EventDispatcherInterface|\PHPUnit\Framework\MockObject\MockObject */
+    private $dispatcher;
+
+    /** @var LoggerInterface|\PHPUnit\Framework\MockObject\MockObject */
+    private $logger;
+
+    /** @var PaymentTransactionInvoiceAction */
+    private $action;
+
+    protected function setUp(): void
+    {
+        $this->contextAccessor = $this->createMock(ContextAccessor::class);
+        $this->paymentMethodProvider = $this->createMock(PaymentMethodProviderInterface::class);
+        $this->paymentTransactionProvider = $this->createMock(PaymentTransactionProvider::class);
+        $this->router = $this->createMock(RouterInterface::class);
+        $this->logger = $this->createMock(LoggerInterface::class);
+        $this->dispatcher = $this->createMock(EventDispatcherInterface::class);
+
+        $this->action = new PaymentTransactionInvoiceAction(
+            $this->contextAccessor,
+            $this->paymentMethodProvider,
+            $this->paymentTransactionProvider,
+            $this->router
+        );
+        $this->action->setLogger($this->logger);
+        $this->action->setDispatcher($this->dispatcher);
+    }
 
     /**
      * @dataProvider executeDataProvider
@@ -25,51 +70,43 @@ class PaymentTransactionInvoiceActionTest extends AbstractActionTest
         $options = $data['options'];
         $context = [];
 
-        $this->contextAccessor
-            ->expects(static::any())
+        $this->contextAccessor->expects(self::any())
             ->method('getValue')
-            ->will($this->returnArgument(1));
+            ->willReturnArgument(1);
 
-        $this->paymentTransactionProvider
-            ->expects(static::atLeastOnce())
+        $this->paymentTransactionProvider->expects(self::atLeastOnce())
             ->method('createPaymentTransactionByParentTransaction')
             ->willReturnMap([
                 [ApruvePaymentMethod::INVOICE, $authorizationPaymentTransaction, $invoicePaymentTransaction],
                 [ApruvePaymentMethod::SHIPMENT, $invoicePaymentTransaction, $shipmentPaymentTransaction],
             ]);
 
-        /** @var PaymentMethodInterface|\PHPUnit\Framework\MockObject\MockObject $paymentMethod */
         $paymentMethod = $this->createMock(PaymentMethodInterface::class);
-        $paymentMethod
-            ->expects(static::atLeastOnce())
+        $paymentMethod->expects(self::atLeastOnce())
             ->method('execute')
             ->willReturnMap([
                 [ApruvePaymentMethod::INVOICE, $invoicePaymentTransaction, $data['invoiceResponse']],
                 [ApruvePaymentMethod::SHIPMENT, $shipmentPaymentTransaction, $data['shipmentResponse']],
             ]);
 
-        $this->paymentMethodProvider
-            ->expects(static::atLeastOnce())
+        $this->paymentMethodProvider->expects(self::atLeastOnce())
             ->method('hasPaymentMethod')
             ->with($authorizationPaymentTransaction->getPaymentMethod())
             ->willReturn(true);
 
-        $this->paymentMethodProvider
-            ->expects(static::atLeastOnce())
+        $this->paymentMethodProvider->expects(self::atLeastOnce())
             ->method('getPaymentMethod')
             ->with($authorizationPaymentTransaction->getPaymentMethod())
             ->willReturn($paymentMethod);
 
-        $this->paymentTransactionProvider
-            ->expects(static::atLeastOnce())
+        $this->paymentTransactionProvider->expects(self::atLeastOnce())
             ->method('savePaymentTransaction')
             ->withConsecutive(
                 [$invoicePaymentTransaction],
                 [$shipmentPaymentTransaction]
             );
 
-        $this->contextAccessor
-            ->expects(static::once())
+        $this->contextAccessor->expects(self::once())
             ->method('setValue')
             ->with($context, $options['attribute'], $expected);
 
@@ -77,24 +114,21 @@ class PaymentTransactionInvoiceActionTest extends AbstractActionTest
         $this->action->execute($context);
     }
 
-    /**
-     * @return array
-     */
-    public function executeDataProvider()
+    public function executeDataProvider(): array
     {
         return [
             'successful' => [
                 'data' => [
-                    'invoicePaymentTransaction' => $this->createPaymentTransaction(10)
+                    'invoicePaymentTransaction' => $this->getPaymentTransaction(10)
                         ->setAction(ApruvePaymentMethod::INVOICE)
                         ->setEntityIdentifier(10)
                         ->setSuccessful(true),
-                    'shipmentPaymentTransaction' => $this->createPaymentTransaction(20)
+                    'shipmentPaymentTransaction' => $this->getPaymentTransaction(20)
                         ->setAction(ApruvePaymentMethod::SHIPMENT)
                         ->setEntityIdentifier(20)
                         ->setSuccessful(true),
                     'options' => [
-                        'paymentTransaction' => $this->createPaymentTransaction(1),
+                        'paymentTransaction' => $this->getPaymentTransaction(1),
                         'attribute' => new PropertyPath('test'),
                         'transactionOptions' => [
                             'testOption' => 'testOption',
@@ -115,13 +149,13 @@ class PaymentTransactionInvoiceActionTest extends AbstractActionTest
             ],
             'invoice is not successful' => [
                 'data' => [
-                    'invoicePaymentTransaction' => $this->createPaymentTransaction(10)
+                    'invoicePaymentTransaction' => $this->getPaymentTransaction(10)
                                                         ->setAction(ApruvePaymentMethod::INVOICE)
                                                         ->setEntityIdentifier(10)
                                                         ->setSuccessful(false),
                     'shipmentPaymentTransaction' => null,
                     'options' => [
-                        'paymentTransaction' => $this->createPaymentTransaction(1),
+                        'paymentTransaction' => $this->getPaymentTransaction(1),
                         'attribute' => new PropertyPath('test'),
                         'transactionOptions' => [
                             'testOption' => 'testOption',
@@ -140,16 +174,16 @@ class PaymentTransactionInvoiceActionTest extends AbstractActionTest
             ],
             'shipment is not successful' => [
                 'data' => [
-                    'invoicePaymentTransaction' => $this->createPaymentTransaction(10)
+                    'invoicePaymentTransaction' => $this->getPaymentTransaction(10)
                                                         ->setAction(ApruvePaymentMethod::INVOICE)
                                                         ->setEntityIdentifier(10)
                                                         ->setSuccessful(true),
-                    'shipmentPaymentTransaction' => $this->createPaymentTransaction(20)
+                    'shipmentPaymentTransaction' => $this->getPaymentTransaction(20)
                                                          ->setAction(ApruvePaymentMethod::SHIPMENT)
                                                          ->setEntityIdentifier(20)
                                                          ->setSuccessful(false),
                     'options' => [
-                        'paymentTransaction' => $this->createPaymentTransaction(1),
+                        'paymentTransaction' => $this->getPaymentTransaction(1),
                         'attribute' => new PropertyPath('test'),
                         'transactionOptions' => [
                             'testOption' => 'testOption',
@@ -172,21 +206,16 @@ class PaymentTransactionInvoiceActionTest extends AbstractActionTest
     }
 
     /**
-     * @param array $options
-     *
      * @dataProvider executeWrongOptionsDataProvider
      */
-    public function testExecuteWrongOptions($options)
+    public function testExecuteWrongOptions(array $options)
     {
-        $this->expectException(\Symfony\Component\OptionsResolver\Exception\UndefinedOptionsException::class);
+        $this->expectException(UndefinedOptionsException::class);
         $this->action->initialize($options);
         $this->action->execute([]);
     }
 
-    /**
-     * @return array
-     */
-    public function executeWrongOptionsDataProvider()
+    public function executeWrongOptionsDataProvider(): array
     {
         return [
             [['someOption' => 'someValue']],
@@ -197,28 +226,12 @@ class PaymentTransactionInvoiceActionTest extends AbstractActionTest
         ];
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    protected function getAction()
+    private function getPaymentTransaction(int $id): PaymentTransaction
     {
-        return new PaymentTransactionInvoiceAction(
-            $this->contextAccessor,
-            $this->paymentMethodProvider,
-            $this->paymentTransactionProvider,
-            $this->router
-        );
-    }
+        $paymentTransaction = new PaymentTransaction();
+        ReflectionUtil::setId($paymentTransaction, $id);
+        $paymentTransaction->setPaymentMethod('testPaymentMethodType');
 
-    /**
-     * @param int $id
-     *
-     * @return PaymentTransaction
-     */
-    private function createPaymentTransaction($id)
-    {
-        $properties = ['id' => $id, 'paymentMethod' => 'testPaymentMethodType'];
-
-        return $this->getEntity(PaymentTransaction::class, $properties);
+        return $paymentTransaction;
     }
 }
