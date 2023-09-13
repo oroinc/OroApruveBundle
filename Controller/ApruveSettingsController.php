cb1fe6d66b524961a04065463063d2298fb2970e
Andrii Yatsenko <ayatsenko@oroinc.com>
2023-09-13 16:43:24 +0300

    BAP-22238: Use Symfony 6 return and param types  (#36400)

--- Controller/ApruveSettingsController.php
+++ Controller/ApruveSettingsController.php
@@ -103,7 +103,7 @@ class ApruveSettingsController extends AbstractController
     /**
      * {@inheritdoc}
      */
-    public static function getSubscribedServices()
+    public static function getSubscribedServices(): array
     {
         return array_merge(
             parent::getSubscribedServices(),
--- Controller/WebhookController.php
+++ Controller/WebhookController.php
@@ -190,7 +190,7 @@ class WebhookController extends AbstractController
     /**
      * {@inheritdoc}
      */
-    public static function getSubscribedServices()
+    public static function getSubscribedServices(): array
     {
         return array_merge(
             parent::getSubscribedServices(),
