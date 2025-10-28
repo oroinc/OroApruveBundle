165b234775cbdd253fe55bc74e4c7109feb20fcf
Alex Yegerev <1755578+lexxcode@users.noreply.github.com>
2025-10-28 22:36:44 +0400

    BB-26295: Convert crm, commerce-crm, commerce-enterprise, authorizenet, apruve, ai-smart-order (#42758)
    * BB-26295: Convert crm, commerce-crm, commerce-enterprise, authorizenet, apruve, ai-smart-order
    
    ---------
    
    Co-authored-by: Valerii Yustyniuk <vyustyniuk@oroinc.com>

--- Resources/public/js/app/components/payment-method-component.js
+++ Resources/public/js/app/components/payment-method-component.js
@@ -1,135 +1,130 @@
 /* global apruve */
-define(function(require) {
-    'use strict';
+import _ from 'underscore';
+import mediator from 'oroui/js/mediator';
+import scriptjs from 'scriptjs';
+import BaseComponent from 'oropayment/js/app/components/payment-method-component';
 
-    const _ = require('underscore');
-    const mediator = require('oroui/js/mediator');
-    const scriptjs = require('scriptjs');
-
-    const BaseComponent = require('oropayment/js/app/components/payment-method-component');
-
-    const PaymentMethodComponent = BaseComponent.extend({
-        /**
-         * @property {Object}
-         */
-        options: {
-            orderIdParamName: '',
-            paymentMethod: null,
-            apruveJsUrls: {
-                test: '//test.apruve.com/js/v4/apruve.js',
-                prod: '//app.apruve.com/js/v4/apruve.js'
-            },
-            testMode: true
+const PaymentMethodComponent = BaseComponent.extend({
+    /**
+     * @property {Object}
+     */
+    options: {
+        orderIdParamName: '',
+        paymentMethod: null,
+        apruveJsUrls: {
+            test: '//test.apruve.com/js/v4/apruve.js',
+            prod: '//app.apruve.com/js/v4/apruve.js'
         },
+        testMode: true
+    },
 
-        apruve: null,
+    apruve: null,
 
-        returnUrl: '',
+    returnUrl: '',
 
-        errorUrl: '',
+    errorUrl: '',
 
-        /**
-         * @inheritdoc
-         */
-        constructor: function PaymentMethodComponent(options) {
-            PaymentMethodComponent.__super__.constructor.call(this, options);
-        },
+    /**
+     * @inheritdoc
+     */
+    constructor: function PaymentMethodComponent(options) {
+        PaymentMethodComponent.__super__.constructor.call(this, options);
+    },
 
-        /**
-         * @inheritdoc
-         */
-        initialize: function(options) {
-            this.options = _.defaults(options || {}, this.options);
+    /**
+     * @inheritdoc
+     */
+    initialize: function(options) {
+        this.options = _.defaults(options || {}, this.options);
 
-            mediator.on('checkout:place-order:response', this.handleSubmit, this);
+        mediator.on('checkout:place-order:response', this.handleSubmit, this);
 
-            this.loadAppruveJsLibrary();
-        },
+        this.loadAppruveJsLibrary();
+    },
 
-        loadAppruveJsLibrary: function() {
-            const appruveJsUrl =
-                this.options.testMode ? this.options.apruveJsUrls.test : this.options.apruveJsUrls.prod;
+    loadAppruveJsLibrary: function() {
+        const appruveJsUrl =
+            this.options.testMode ? this.options.apruveJsUrls.test : this.options.apruveJsUrls.prod;
 
-            this._deferredInit();
-            scriptjs(appruveJsUrl, this.initializeApruve.bind(this));
-        },
+        this._deferredInit();
+        scriptjs(appruveJsUrl, this.initializeApruve.bind(this));
+    },
 
-        initializeApruve: function() {
-            this.apruve = apruve;
+    initializeApruve: function() {
+        this.apruve = apruve;
 
-            this.apruve
-                .registerApruveCallback(this.apruve.APRUVE_LAUNCHED_EVENT, this.handleApruveLaunch.bind(this));
-            this.apruve
-                .registerApruveCallback(this.apruve.APRUVE_COMPLETE_EVENT, this.handleApruveComplete.bind(this));
-            this.apruve
-                .registerApruveCallback(this.apruve.APRUVE_CLOSED_EVENT, this.handleApruveClose.bind(this));
+        this.apruve
+            .registerApruveCallback(this.apruve.APRUVE_LAUNCHED_EVENT, this.handleApruveLaunch.bind(this));
+        this.apruve
+            .registerApruveCallback(this.apruve.APRUVE_COMPLETE_EVENT, this.handleApruveComplete.bind(this));
+        this.apruve
+            .registerApruveCallback(this.apruve.APRUVE_CLOSED_EVENT, this.handleApruveClose.bind(this));
 
-            this._resolveDeferredInit();
-        },
+        this._resolveDeferredInit();
+    },
 
-        /**
-         * @param {Object} eventData
-         */
-        handleSubmit: function(eventData) {
-            if (eventData.responseData.paymentMethod === this.options.paymentMethod) {
-                eventData.stopped = true;
+    /**
+     * @param {Object} eventData
+     */
+    handleSubmit: function(eventData) {
+        if (eventData.responseData.paymentMethod === this.options.paymentMethod) {
+            eventData.stopped = true;
 
-                const responseData = _.extend({successUrl: this.getSuccessUrl()}, eventData.responseData);
+            const responseData = _.extend({successUrl: this.getSuccessUrl()}, eventData.responseData);
 
-                if (!responseData.apruveOrder) {
-                    mediator.execute('redirectTo', {url: this.errorUrl}, {redirect: true});
+            if (!responseData.apruveOrder) {
+                mediator.execute('redirectTo', {url: this.errorUrl}, {redirect: true});
 
-                    return;
-                }
-
-                this.returnUrl = responseData.returnUrl;
-                this.errorUrl = responseData.errorUrl;
-
-                const self = this;
-                // Ensure that apruve library is loaded before starting apruve checkout.
-                this.deferredInit.done(function() {
-                    // Provide order object and secure hash to apruve.
-                    self.apruve.setOrder(responseData.apruveOrder, responseData.apruveOrderSecureHash);
-
-                    self.apruve.startCheckout();
-                });
-            }
-        },
-
-        /**
-         * Hide loading mask only when apruve popup is fully loaded.
-         */
-        handleApruveLaunch: function() {
-            mediator.execute('hideLoading');
-        },
-
-        /**
-         * @param {String} orderId
-         */
-        handleApruveComplete: function(orderId) {
-            mediator.execute('showLoading');
-            mediator.execute(
-                'redirectTo',
-                {url: this.returnUrl + '?' + this.options.orderIdParamName + '=' + orderId},
-                {redirect: true}
-            );
-        },
-
-        handleApruveClose: function() {
-            mediator.execute('showLoading');
-            mediator.execute('redirectTo', {url: this.errorUrl}, {redirect: true});
-        },
-
-        dispose: function() {
-            if (this.disposed) {
                 return;
             }
 
-            mediator.off(null, null, this);
+            this.returnUrl = responseData.returnUrl;
+            this.errorUrl = responseData.errorUrl;
 
-            PaymentMethodComponent.__super__.dispose.call(this);
+            const self = this;
+            // Ensure that apruve library is loaded before starting apruve checkout.
+            this.deferredInit.done(function() {
+                // Provide order object and secure hash to apruve.
+                self.apruve.setOrder(responseData.apruveOrder, responseData.apruveOrderSecureHash);
+
+                self.apruve.startCheckout();
+            });
         }
-    });
+    },
 
-    return PaymentMethodComponent;
+    /**
+     * Hide loading mask only when apruve popup is fully loaded.
+     */
+    handleApruveLaunch: function() {
+        mediator.execute('hideLoading');
+    },
+
+    /**
+     * @param {String} orderId
+     */
+    handleApruveComplete: function(orderId) {
+        mediator.execute('showLoading');
+        mediator.execute(
+            'redirectTo',
+            {url: this.returnUrl + '?' + this.options.orderIdParamName + '=' + orderId},
+            {redirect: true}
+        );
+    },
+
+    handleApruveClose: function() {
+        mediator.execute('showLoading');
+        mediator.execute('redirectTo', {url: this.errorUrl}, {redirect: true});
+    },
+
+    dispose: function() {
+        if (this.disposed) {
+            return;
+        }
+
+        mediator.off(null, null, this);
+
+        PaymentMethodComponent.__super__.dispose.call(this);
+    }
 });
+
+export default PaymentMethodComponent;
--- Resources/public/js/app/components/webhook-token-component.js
+++ Resources/public/js/app/components/webhook-token-component.js
@@ -1,117 +1,113 @@
-define(function(require) {
-    'use strict';
+import $ from 'jquery';
+import _ from 'underscore';
+import routing from 'routing';
+import BaseComponent from 'oroui/js/app/components/base/component';
+import LoadingMaskView from 'oroui/js/app/views/loading-mask-view';
 
-    const $ = require('jquery');
-    const _ = require('underscore');
-    const routing = require('routing');
-    const BaseComponent = require('oroui/js/app/components/base/component');
-    const LoadingMaskView = require('oroui/js/app/views/loading-mask-view');
+const WebhookTokenComponent = BaseComponent.extend({
+    options: {
+        generateTokenRoute: 'oro_apruve_generate_token',
+        webhookRoute: 'oro_apruve_webhook_notify'
+    },
 
-    const WebhookTokenComponent = BaseComponent.extend({
-        options: {
-            generateTokenRoute: 'oro_apruve_generate_token',
-            webhookRoute: 'oro_apruve_webhook_notify'
-        },
+    /**
+     * @inheritdoc
+     */
+    constructor: function WebhookTokenComponent(options) {
+        WebhookTokenComponent.__super__.constructor.call(this, options);
+    },
 
-        /**
-         * @inheritdoc
-         */
-        constructor: function WebhookTokenComponent(options) {
-            WebhookTokenComponent.__super__.constructor.call(this, options);
-        },
+    /**
+     * @inheritdoc
+     */
+    initialize: function(options) {
+        this.options = _.defaults(options || {}, this.options);
 
-        /**
-         * @inheritdoc
-         */
-        initialize: function(options) {
-            this.options = _.defaults(options || {}, this.options);
+        this.$button = options._sourceElement;
+        this.$webhookToken = $(options.webhookTokenSelector);
+        this.$webhookUrl = $(options.webhookUrlSelector);
+        this.loadingMaskView = new LoadingMaskView({container: $('body')});
 
-            this.$button = options._sourceElement;
-            this.$webhookToken = $(options.webhookTokenSelector);
-            this.$webhookUrl = $(options.webhookUrlSelector);
-            this.loadingMaskView = new LoadingMaskView({container: $('body')});
+        this.initListeners();
 
-            this.initListeners();
+        this.setWebhookUrl(this.buildWebhookUrl(this.getWebhookToken()));
+    },
 
-            this.setWebhookUrl(this.buildWebhookUrl(this.getWebhookToken()));
-        },
+    initListeners: function() {
+        this.$button.on('click', this.buttonClickHandler.bind(this));
+    },
 
-        initListeners: function() {
-            this.$button.on('click', this.buttonClickHandler.bind(this));
-        },
+    buttonClickHandler: function() {
+        const self = this;
 
-        buttonClickHandler: function() {
-            const self = this;
+        this.fetchToken(function(token) {
+            self.setWebhookToken(token);
+            self.setWebhookUrl(self.buildWebhookUrl(token));
+        });
+    },
 
-            this.fetchToken(function(token) {
-                self.setWebhookToken(token);
-                self.setWebhookUrl(self.buildWebhookUrl(token));
-            });
-        },
+    /**
+     * @param {Function} fetchTokenCallback
+     */
+    fetchToken: function(fetchTokenCallback) {
+        const self = this;
 
-        /**
-         * @param {Function} fetchTokenCallback
-         */
-        fetchToken: function(fetchTokenCallback) {
-            const self = this;
-
-            $.ajax({
-                url: routing.generate(this.options.generateTokenRoute),
-                type: 'POST',
-                beforeSend: function() {
-                    self.loadingMaskView.show();
-                },
-                success: function(response) {
-                    const token = response.token || '';
-                    if (token) {
-                        fetchTokenCallback(token);
-                    }
-                },
-                complete: function() {
-                    self.loadingMaskView.hide();
+        $.ajax({
+            url: routing.generate(this.options.generateTokenRoute),
+            type: 'POST',
+            beforeSend: function() {
+                self.loadingMaskView.show();
+            },
+            success: function(response) {
+                const token = response.token || '';
+                if (token) {
+                    fetchTokenCallback(token);
                 }
-            });
-        },
-
-        /**
-         * @param {String} token
-         * @returns {String}
-         */
-        buildWebhookUrl: function(token) {
-            return routing.generate(this.options.webhookRoute, {token: token}, true);
-        },
-
-        /**
-         * @param {String} url
-         */
-        setWebhookUrl: function(url) {
-            this.$webhookUrl.text(url);
-        },
-
-        /**
-         * @param {String} token
-         */
-        setWebhookToken: function(token) {
-            this.$webhookToken.val(token);
-        },
-
-        /**
-         * @returns {String}
-         */
-        getWebhookToken: function() {
-            return this.$webhookToken.val();
-        },
-
-        dispose: function() {
-            if (this.disposed) {
-                return;
+            },
+            complete: function() {
+                self.loadingMaskView.hide();
             }
+        });
+    },
 
-            this.$button.off('click');
+    /**
+     * @param {String} token
+     * @returns {String}
+     */
+    buildWebhookUrl: function(token) {
+        return routing.generate(this.options.webhookRoute, {token: token}, true);
+    },
 
-            WebhookTokenComponent.__super__.dispose.call(this);
+    /**
+     * @param {String} url
+     */
+    setWebhookUrl: function(url) {
+        this.$webhookUrl.text(url);
+    },
+
+    /**
+     * @param {String} token
+     */
+    setWebhookToken: function(token) {
+        this.$webhookToken.val(token);
+    },
+
+    /**
+     * @returns {String}
+     */
+    getWebhookToken: function() {
+        return this.$webhookToken.val();
+    },
+
+    dispose: function() {
+        if (this.disposed) {
+            return;
         }
-    });
 
-    return WebhookTokenComponent;
+        this.$button.off('click');
+
+        WebhookTokenComponent.__super__.dispose.call(this);
+    }
 });
+
+export default WebhookTokenComponent;
